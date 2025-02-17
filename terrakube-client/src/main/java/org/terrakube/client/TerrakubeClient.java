8cd7e802a3da1674b306ca53ceb155c19671c865
roche-quentin <roche.quentin@bcg.com>
2025-02-17 10:17:04 +0100

    change post output to string

--- terrakube-client/src/main/java/org/terrakube/client/TerrakubeClient.java
+++ terrakube-client/src/main/java/org/terrakube/client/TerrakubeClient.java
@@ -1,6 +1,5 @@
 package org.terrakube.client;
 
-import feign.Body;
 import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
@@ -8,6 +7,8 @@ import org.terrakube.client.model.organization.Organization;
 import org.terrakube.client.model.organization.job.Job;
 import org.terrakube.client.model.organization.job.JobRequest;
 import org.terrakube.client.model.organization.job.LogsRequest;
+import org.terrakube.client.model.organization.job.step.TfOutput;
+import org.terrakube.client.model.organization.job.step.TfOutputPath;
 import org.terrakube.client.model.organization.job.step.Step;
 import org.terrakube.client.model.organization.job.step.StepRequest;
 import org.terrakube.client.model.organization.module.Module;
@@ -118,8 +119,8 @@ public interface TerrakubeClient {
 
     @RequestLine("POST /tfoutput/v1/organization/{organizationId}/job/{jobId}/step/{stepId}")
     @Headers("Content-Type: application/vnd.api+json")
-    Response<String> uploadOutput(
-            String tfOutput,
+    Response<TfOutputPath> uploadOutput(
+            TfOutput tfOutput,
             @Param("organizationId") String organizationId,
             @Param("jobId") String jobId,
             @Param("stepId") String stepId
