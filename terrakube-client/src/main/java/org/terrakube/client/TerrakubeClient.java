5b080157f9eff82bd6d2633e911e7c0b2f60f0e1
roche-quentin <roche.quentin@bcg.com>
2025-02-17 13:13:43 +0100

    change post output to string

--- terrakube-client/src/main/java/org/terrakube/client/TerrakubeClient.java
+++ terrakube-client/src/main/java/org/terrakube/client/TerrakubeClient.java
@@ -115,8 +115,8 @@ public interface TerrakubeClient {
 
     @RequestLine("POST /tfoutput/v1/organization/{organizationId}/job/{jobId}/step/{stepId}")
     @Headers("Content-Type: application/vnd.api+json")
-    Response<TfOutputUrl> uploadOutput(
-            TfOutputRequest tfOutput,
+    Response<String> uploadOutput(
+            String tfOutput,
             @Param("organizationId") String organizationId,
             @Param("jobId") String jobId,
             @Param("stepId") String stepId
